9069d7bbdc9f4febac9f5d4a97d049f594422f81
Zack Di Salvo <138753379+zdisalvo@users.noreply.github.com>
2024-07-19 00:29:21 -0700

    saving messages2

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,23 +1,23 @@
 import { Box, Container, Flex, Skeleton, SkeletonCircle, Text, VStack } from "@chakra-ui/react";
 import useAuthStore from "../../store/authStore";
-import useGetSparkProfileById from "../../hooks/useGetSparkProfileById";
-import Message from "./Message";
-//import useGetSparkMatchesById from "../../hooks/useGetSparkMatchesById";
 import useGetMessagesById from "../../hooks/useGetMessagesById";
 import Convo from "./Convo";
 
-
 const Messages = () => {
-    const authUser = useAuthStore((state) => state.user);
-    //const { isLoading: profileLoading, sparkProfile } = useGetSparkProfileById(authUser?.uid);
-  //const sparkProfile = useSparkProfileStore((state) => state.setSparkProfile);
+  const authUser = useAuthStore((state) => state.user);
   const { isLoading, messages } = useGetMessagesById(authUser?.uid);
-  //const [viewedPosts, setViewedPosts] = useState([]);
 
+  // Sort conversations by the most recent last message
+  const sortedMessages = messages.slice().sort((a, b) => {
+    const lastMessageA = a.messages[a.messages.length - 1];
+    const lastMessageB = b.messages[b.messages.length - 1];
+    
+    return (lastMessageB?.timeStamp?.seconds || 0) - (lastMessageA?.timeStamp?.seconds || 0);
+  });
 
   return (
-    <Container py={6}   px={0} w={['100vw', null, '80vh']} >
-      {isLoading &&
+    <Container py={6} px={0} w={['100vw', null, '80vh']}>
+      {isLoading && 
         [0, 1, 2, 3, 4].map((_, idx) => (
           <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
             <Flex gap='2'>
@@ -32,10 +32,10 @@ const Messages = () => {
             </Skeleton>
           </VStack>
         ))}
-      
-      {!isLoading && messages.length > 0 && messages.map((convo) => <Convo key={convo.receivingUserId} userId={authUser.uid} receivingUserId={convo.receivingUserId} />)}
-      
-      
+
+      {!isLoading && sortedMessages.length > 0 && sortedMessages.map((convo) => (
+        <Convo key={convo.receivingUserId} userId={authUser.uid} receivingUserId={convo.receivingUserId} />
+      ))}
     </Container>
   );
 };
