980eb64b761d9dfece905c1d006a0e258b6b0b89
Zack Di Salvo <138753379+zdisalvo@users.noreply.github.com>
2024-07-19 20:33:30 -0700

    saving follow notifications to go back to profile header functionality

--- src/hooks/useFollowUser.js
+++ src/hooks/useFollowUser.js
@@ -3,7 +3,7 @@ import useAuthStore from "../store/authStore";
 import useUserProfileStore from "../store/userProfileStore";
 import useShowToast from "./useShowToast";
 import { firestore } from "../firebase/firebase";
-import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { arrayRemove, arrayUnion, doc, updateDoc, setDoc, getDoc } from "firebase/firestore";
 
 const useFollowUser = (userId) => {
 	const [isUpdating, setIsUpdating] = useState(false);
@@ -14,29 +14,43 @@ const useFollowUser = (userId) => {
 	const showToast = useShowToast();
 
 	const handleFollowUser = async () => {
+		if (isUpdating) return;
 		setIsUpdating(true);
+
 		try {
 			const currentUserRef = doc(firestore, "users", authUser.uid);
-			const userToFollowOrUnfollorRef = doc(firestore, "users", userId);
-			await updateDoc(currentUserRef, {
-				following: isFollowing ? arrayRemove(userId) : arrayUnion(userId),
-			});
-
-			await updateDoc(userToFollowOrUnfollorRef, {
-				followers: isFollowing ? arrayRemove(authUser.uid) : arrayUnion(authUser.uid),
-			});
+			const userToFollowOrUnfollowRef = doc(firestore, "users", userId);
 
 			if (isFollowing) {
-				// unfollow
+				// Unfollow
+				await updateDoc(currentUserRef, {
+					following: arrayRemove(userId),
+				});
+				await updateDoc(userToFollowOrUnfollowRef, {
+					followers: arrayRemove(authUser.uid),
+				});
+
+				// Remove notification
+				const userNotificationsRef = doc(firestore, "users", userId);
+				const userNotificationsSnap = await getDoc(userNotificationsRef);
+				const notifications = userNotificationsSnap.exists()
+					? userNotificationsSnap.data().notifications || []
+					: [];
+				const updatedNotifications = notifications.filter(
+					(notification) => !(notification.userId === authUser.uid && notification.type === "follow")
+				);
+				await setDoc(userNotificationsRef, { notifications: updatedNotifications }, { merge: true });
+
 				setAuthUser({
 					...authUser,
 					following: authUser.following.filter((uid) => uid !== userId),
 				});
-				if (userProfile)
+				if (userProfile) {
 					setUserProfile({
 						...userProfile,
 						followers: userProfile.followers.filter((uid) => uid !== authUser.uid),
 					});
+				}
 
 				localStorage.setItem(
 					"user-info",
@@ -46,18 +60,40 @@ const useFollowUser = (userId) => {
 					})
 				);
 				setIsFollowing(false);
+				
 			} else {
-				// follow
+				// Follow
+				await updateDoc(currentUserRef, {
+					following: arrayUnion(userId),
+				});
+				await updateDoc(userToFollowOrUnfollowRef, {
+					followers: arrayUnion(authUser.uid),
+				});
+
+				// Add notification
+				const userNotificationsRef = doc(firestore, "users", userId);
+				const notification = {
+					userId: authUser.uid,
+					time: new Date(),
+					type: "follow",
+				};
+				const userNotificationsSnap = await getDoc(userNotificationsRef);
+				const notifications = userNotificationsSnap.exists()
+					? userNotificationsSnap.data().notifications || []
+					: [];
+				notifications.push(notification);
+				await setDoc(userNotificationsRef, { notifications }, { merge: true });
+
 				setAuthUser({
 					...authUser,
 					following: [...authUser.following, userId],
 				});
-
-				if (userProfile)
+				if (userProfile) {
 					setUserProfile({
 						...userProfile,
 						followers: [...userProfile.followers, authUser.uid],
 					});
+				}
 
 				localStorage.setItem(
 					"user-info",
