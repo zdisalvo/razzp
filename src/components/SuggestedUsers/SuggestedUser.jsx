cc006c710da11bfc02b7e54f3900b28e05885670
Zack Di Salvo <138753379+zdisalvo@users.noreply.github.com>
2024-07-29 00:41:54 -0700

    saving suggestedUser in search

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,21 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react";
 import useFollowUser from "../../hooks/useFollowUser";
 import useAuthStore from "../../store/authStore";
 import { Link } from "react-router-dom";
+import useFollowUserFP from "../../hooks/useFollowUserFP";
 
 const SuggestedUser = ({ user, setUser }) => {
-	const { isFollowing, isUpdating, handleFollowUser } = useFollowUser(user.uid);
+	const { isFollowing, isUpdating } = useFollowUser(user.uid);
+	const { handleFollowUser } = useFollowUserFP();
 	const authUser = useAuthStore((state) => state.user);
+	const [isFollowingInit, setIsFollowingInit] = useState(isFollowing);
 
-	const onFollowUser = async () => {
-		await handleFollowUser();
-		setUser({
-			...user,
-			followers: isFollowing
-				? user.followers.filter((follower) => follower.uid !== authUser.uid)
-				: [...user.followers, authUser],
-		});
-	};
+	useEffect(() => {
+        setIsFollowingInit(isFollowing);
+    }, [isFollowing]);
+
+	// const onFollowUser = async () => {
+	// 	await handleFollowUser();
+	// 	setUser({
+	// 		...user,
+	// 		followers: isFollowing
+	// 			? user.followers.filter((follower) => follower.uid !== authUser.uid)
+	// 			: [...user.followers, authUser],
+	// 	});
+	// };
+
+	const handleFollowClick = async (userId) => {
+        const isCurrentlyFollowing = isFollowingInit;
+        
+        // Optimistically update the state
+        setIsFollowingInit(!isCurrentlyFollowing);
+        
+        try {
+            await handleFollowUser(userId, isCurrentlyFollowing);
+        } catch (error) {
+            console.error('Error updating follow status:', error);
+            // Rollback optimistic update in case of error
+            setIsFollowingInit(isCurrentlyFollowing);
+        }
+    };
 
 	return (
 		<Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
@@ -47,20 +70,16 @@ const SuggestedUser = ({ user, setUser }) => {
 			</Flex>
 			{authUser.uid !== user.uid && (
 				<Button
-					fontSize={13}
-					bg={"transparent"}
-					p={0}
-					mr={6}
-					h={"max-content"}
-					fontWeight={"medium"}
-					color={"blue.400"}
-					cursor={"pointer"}
-					_hover={{ color: "white" }}
-					onClick={onFollowUser}
-					isLoading={isUpdating}
-				>
-					{isFollowing ? "Unfollow" : "Follow"}
-				</Button>
+				ml="auto"
+				onClick={() => handleFollowClick(user.uid)}
+				bg={"#eb7734"}
+				color={"white"}
+				textShadow="2px 2px 4px rgba(0, 0, 0, 0.5)"
+				_hover={{ bg: "#c75e1f" }}
+				size={{ base: "sm", md: "sm" }}
+			>
+				{isFollowingInit ? 'Unfollow' : 'Follow'}
+			</Button>
 			)}
 		</Flex>
 	);
